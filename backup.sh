c563523393dffd8d438aeba24007cf00520703fb
Carlos Sanchez <carlosalvet@gmail.com>
2020-09-18 16:41:21 -0500

    [FIX ] Se usa el comando uchardet para determinar el tipo de codificación en el archivo

---
+++ backup.sh
@@ -0,0 +1,82 @@
+
+ftp_source() {
+	local ftp_host="ftp://fulloffice.com.mx"
+	local file_uri="Lista%20de%20Precios%20101444.csv"
+	local host_user='fulloffice';
+	local host_password='Fulloffice2020!'
+	local local_file=$1;
+
+	local destiny_folder=`dirname $local_file`
+	mkdir $destiny_folder &> /dev/null
+
+	curl_parameters="${ftp_host}/${file_uri} --user ${host_user}:${host_password}"
+	output_file=`curl  $curl_parameters > $local_file`
+
+	if [ $? != 0 ]; then
+		return 1
+	fi
+	
+	echo $output_file
+}
+
+encodetype() {
+	local filepath=$1
+	#local encode_type=`file -bi $filepath  |cut -d " " -f 2 | cut -d "=" -f 2`
+	local enconde_type=`uchardet $filepath`
+	echo $encode_type;
+	exit 2;
+	iconv --list | grep "$encode_type"
+    if [ $? != 0 ]; then
+		echo $2
+		return 1
+	fi
+
+ 	echo $encode_type
+
+}
+
+
+convert_utf8() {
+	local encode_type=$1
+	local origin=$2
+	local basename=`basename $origin`
+	local destiny_folder=$3
+	
+	local destiny="${destiny_folder}/utf8.${basename}"
+
+	iconv -f $encode_type -t utf-8 -o $destiny $origin
+
+	if [ $? != 0 ]; then
+		return 1
+	fi
+
+	echo "Se generó el archivo $destiny"
+}
+
+
+temp_file[0]="../temp/ct.lista-productos"
+temp_file[1]="../temp/dc.lista-productos"
+destiny_folder='../private'
+encode_default='ISO_8859-5'
+
+ftp_source ${temp_file[0]};
+if [ $? == 1 ]; then
+	echo "error $? al obtener de ftp: no se pudo crear el archivo $loca_file"
+	exit 1
+fi
+
+encode_type=`encodetype ${temp_file[0]} $encode_default` 
+if [ $? == 1 ]; then
+	echo "WARNING: El formato del archivo no es reconocido $file_encode  se usará $encode_default en su lugar"
+fi
+
+convert_utf8 $encode_type ${temp_file[0]} $destiny_folder
+if [ $? == 1 ]; then
+	echo "error $?: no se pudo crear el archivo utf8"
+	exit 2 
+fi
+	
+
+
+
+
--- test.sh
+++ test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-source read_ini.sh
-
+#source read_ini.sh
 
 ftp_source() {
 	local ftp_host="ftp://fulloffice.com.mx"
@@ -9,8 +8,8 @@ ftp_source() {
 	local host_user='fulloffice';
 	local host_password='Fulloffice2020!'
 	local local_file=$1;
-
 	local destiny_folder=`dirname $local_file`
+
 	mkdir $destiny_folder &> /dev/null
 
 	curl_parameters="${ftp_host}/${file_uri} --user ${host_user}:${host_password}"
@@ -25,59 +24,60 @@ ftp_source() {
 
 encodetype() {
 	local filepath=$1
-	local encode_type=`file -bi $filepath  |cut -d " " -f 2 | cut -d "=" -f 2`
-	iconv --list | grep "$encode_type"
+	local encode_type=`uchardet $filepath`
+
+	iconv --list | grep $encode_type > /dev/null
     if [ $? != 0 ]; then
-		echo $2
 		exit 1
 	fi
 
  	echo $encode_type
-
 }
 
 
 convert_utf8() {
 	local encode_type=$1
+
 	local origin=$2
 	local basename=`basename $origin`
+
 	local destiny_folder=$3
-	
 	local destiny="${destiny_folder}/utf8.${basename}"
 
 	iconv -f $encode_type -t utf-8 -o $destiny $origin
-
 	if [ $? != 0 ]; then
 		exit 1
 	fi
-
-	echo "Se generó el archivo utf8 en $destiny"
+	echo $destiny
 }
 
 
-temp_file[0]="../temp/ct.lista-productos"
-temp_file[1]="../temp/dc.lista-productos"
+filename_from_ftp[0]="../temp/dc.lista-productos"
+filename_from_ftp[1]="../temp/ct.lista-productos"
 destiny_folder='../private'
-encode_default='ISO_8859-5'
+declare -A ftp_host
+ftp_host[1]['name']="ftp://fulloffice.com.mx"
+file_uri="Lista%20de%20Precios%20101444.csv"
+host_user='fulloffice';
+host_password='Fulloffice2020!'
 
-ftp_source ${temp_file[0]};
+ftp_source ${filename_from_ftp[0]};
 if [ $? == 1 ]; then
-	echo "error $? al obtener de ftp: no se pudo crear el archivo $loca_file"
+	echo "ERROR: $? al obtener de ftp: no se pudo crear el archivo $loca_file"
 	exit 1
 fi
+echo "Se obtuvó el archivo ${filename_from_ftp[0]}"
 
-encode_type=`encodetype ${temp_file[0]} $encode_default` 
+encode_type=`encodetype ${filename_from_ftp[0]}` 
 if [ $? == 1 ]; then
-	echo "WARNING: El formato del archivo no es reconocido $file_encode  se usará $encode_default en su lugar"
+	echo "ERROR: Códificación de archivo no soportada $encode_type"
+	exit 2
 fi
+echo "La codificación del archivo es $encode_type"
 
-convert_utf8 $encode_type ${temp_file[0]} $destiny_folder
+filename=`convert_utf8 $encode_type ${filename_from_ftp[0]} $destiny_folder`
 if [ $? == 1 ]; then
-	echo "error $?: no se pudo crear el archivo utf8"
+	echo "ERROR $?: no se pudo crear el archivo utf8"
 	exit 3 
 fi
-	
-
-
-
-
+echo "Se generó el archivo $filename"
